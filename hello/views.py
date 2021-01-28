dfab8a799ac126495a685898efeecfe2c479f809
Matt Langeman <matt@langeman.net>
2021-01-27 20:11:32 -0500

    reading for qa 0.1.0

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World! Welcome to the web.")
+    return HttpResponse("Hello, World! Welcome to the web. Tagged qa-0.1.0")
