3d02e3330296cb294d7baf7d18405acaaa7ca241
Matt Langeman <matt@langeman.net>
2021-02-02 15:52:23 -0500

    add database settings

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -74,18 +74,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_hello.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-"""
-DATABASES = {    
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }    
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PW'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
+   }
 }
-"""
 
 
 # Password validation
