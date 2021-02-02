6f85888f5896f70296f82fcef484541f1bfba97c
Matt Langeman <matt@langeman.net>
2021-02-02 13:53:22 -0500

    adding html template and testing ways to set env vars

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 DJANGO_GETS_ENV = config('DJANGO_GETS_ENV')
-
+DJANGO_OTHER_ENV_VAR = config('DJANGO_OTHER_ENV_VAR')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
 from django.conf import settings
 
 def hello(request):
-    return HttpResponse("Hello, World! %s" % (settings.DJANGO_GETS_ENV))
+    context = {
+        'DJANGO_GETS_ENV': settings.DJANGO_GETS_ENV,
+        'DJANGO_OTHER_ENV_VAR': settings.DJANGO_OTHER_ENV_VAR
+    }
+    response = TemplateResponse(request, 'hello/hello.html', context)
+    return response
