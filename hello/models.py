eccaf233ae742ef3b5d14de0ce1cdf6d1ccb78e0
Matt Langeman <matt@langeman.net>
2021-02-05 15:02:29 -0500

    add upload_to path for file uploads

--- hello/models.py
+++ hello/models.py
@@ -3,5 +3,5 @@ from django.db import models
 from django_hello.backend import PrivateStorage, PublicStorage
 
 class FileUpload(models.Model):
-    private_upload = models.FileField(storage=PrivateStorage, null=True, blank=True)
-    public_upload = models.FileField(storage=PublicStorage, null=True, blank=True)
+    private_upload = models.FileField(upload_to='private_uploads', storage=PrivateStorage, null=True, blank=True)
+    public_upload = models.FileField(upload_to='public_uploads', storage=PublicStorage, null=True, blank=True)
