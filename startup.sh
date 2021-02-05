95fbca1692e04aa3470f52119a7b6e3de59d4529
Matt Langeman <matt@langeman.net>
2021-02-05 09:14:15 -0500

    add migration and collect static to startupscript

--- startup.sh
+++ startup.sh
@@ -1,3 +1,8 @@
+echo "-- Migrate Django Data Models"
+python manage.py migrate
+echo "-- Collect Static Files"
+python manage.py collectstatic --no-input
+echo "-- Start gunicorn with uvicorn workers"
 gunicorn --workers 8 --threads 4 --timeout 60 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000  -k uvicorn.workers.UvicornWorker \
      --chdir=/home/site/wwwroot django_hello.asgi
